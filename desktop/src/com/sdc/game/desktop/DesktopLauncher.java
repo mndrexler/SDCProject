b4fd456aabe9355e90f3585c694e60e81c17d8c0
mndrexler <mndrexler@wisc.edu>
2021-10-14 00:10:50 -0500

    Add libGDX library

---
+++ desktop/src/com/sdc/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sdc.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sdc.game.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
